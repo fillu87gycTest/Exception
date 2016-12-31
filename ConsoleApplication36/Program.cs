537b89602c2195030447d4f828a2d837e3a21804
fillu87gyc <wander.off65@gmail.com>
2017-01-01 03:06:12 +0900

    完成

--- ConsoleApplication36/Program.cs
+++ ConsoleApplication36/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication36
+/*
+ * Before os generated
+ * Standard message is :
+ * System.IndexOutOfRangeException: インデックスが配列の境界外です。
+ *    場所 ExcTest.GenException() 場所 D:\Program\Cs\ConsoleApplication36\ConsoleApplication36\ExcTest.cs:行 10
+ *    場所 UseException.Main() 場所 D:\Program\Cs\ConsoleApplication36\ConsoleApplication36\Program.cs:行 9
+ * Stack trace :    
+ *    場所 ExcTest.GenException() 場所 D:\Program\Cs\ConsoleApplication36\ConsoleApplication36\ExcTest.cs:行 10
+ *    場所 UseException.Main() 場所 D:\Program\Cs\ConsoleApplication36\ConsoleApplication36\Program.cs:行 9
+ * Mmessage : インデックスが配列の境界外です。
+ * TragetSite : Void GenException()
+ * 
+ * After try/catch block
+ * 続行するには何かキーを押してください . . . 
+ */
+
+class UseException
 {
-	class Program
+	static void Main()
 	{
-		static void Main(string[] args)
+		try
 		{
+			ExcTest.GenException();
 		}
+		catch (IndexOutOfRangeException exc)
+		{
+			Console.WriteLine("Standard message is : ");
+			Console.WriteLine(exc);
+			Console.WriteLine("Stack trace : " + exc.StackTrace);
+			Console.WriteLine("Mmessage : "+ exc.Message);
+			Console.WriteLine("TragetSite : "+exc.TargetSite);
+			Console.WriteLine();
+		}
+		Console.WriteLine("After try/catch block");
 	}
 }
